c21fb61b1c68f0ef53de40e1db90209f84b9013b
Josh Lee <josh@floor.is>
2017-07-13 19:12:42 -0700

    merge license files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 with open('README.rst') as f:
     readme = f.read()
 
-with open('LICENSE.txt') as f:
+with open('LICENSE') as f:
     license = f.read()
 
 setup(
